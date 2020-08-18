84d434ffc084b27df1a8c26a38ae287b4c59694b
Marian Nociar <majo.nociar@gmail.com>
2020-08-18 11:22:17 +0200

    handling POST requests

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 basic project structure 
 ### Lesson 2
 using MVC (Model, View, Controller) approach + Routes
+### Lesson 2
+handling POST requests
--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const authRouter = require('./routes/authRoutes');
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/public', express.static('public'));
 
 app.set('view engine', 'ejs');
