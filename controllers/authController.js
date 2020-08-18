8121d3eb7417cb3b436caf3b476f2b2305f1cad1
Marian Nociar <majo.nociar@gmail.com>
2020-08-18 16:58:52 +0200

    mongoose validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,26 @@
 const User = require('../models/user');
 
+// handle errors
+const handleErrors = (err) => {
+    console.log(err.message, err.code)
+    let errors = {
+        email: "",
+        password: ""
+    };
+
+    // validations errors
+    if(err.message.includes('user validation failed:')){
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message;
+        });
+    }
+    // duplicate error code
+    if(err.code === 11000){
+        errors.email = "that email is already registered";
+    }
+    return errors;
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -13,8 +34,8 @@ module.exports.signup_post = async (req, res) => {
         res.status(201).json(user);
     }
     catch(err){
-        console.log(err);
-        res.status(400).send('Error, user not created');
+        const errors = handleErrors(err);
+        res.status(400).json({errors});
     } 
 }
 module.exports.login_post = async (req, res) => {
