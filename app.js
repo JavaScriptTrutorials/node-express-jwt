523c7e3a4f22a492c4cee5125fb921f7f378c4c4
Marian Nociar <majo.nociar@gmail.com>
2020-08-23 17:54:24 +0200

    cookies

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const {PORT, DB} = require('./config');
 const authRouter = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/public', express.static('public'));
+app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 
@@ -23,6 +25,20 @@ app.get('/', (req, res) => {
 
 app.get('/smoothies', (req, res) => {
     res.render('./smoothies')
-})
+});
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+    //res.setHeader('Set-Cookie', 'newUser=True');
+    res.cookie('newUser', false);
+    res.cookie('isEmploy', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true});
+    res.send('you got the cookies!');
+});
+
+app.get('/get-cookies', (req, res) => {
+    const cookies = req.cookies;
+    console.log(cookies);
+    res.json(cookies);
+});
 
 app.use(authRouter);
